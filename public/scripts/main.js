d7c18e9660006f787770003dad05ed5dab5219b1
Andre Hinton <andre.hinton@vizientinc.com>
2024-05-26 22:13:15 -0500

    fixed line still need to clean up code but we're good

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 const width = window.innerWidth - 20;
 const height = window.innerHeight - 20;
-const radius = Math.min(width, height) / 2;
+const radius = Math.min(width, height) / 2 - 50;
 const tooltip = d3.select("#tooltip");
 
 //create a pages array
@@ -11,6 +11,7 @@ let filteredData;
 let csvData;
 let arc;
 let arcHover;
+let outerArc;
 
 //load the data and store it in a variable
 d3.csv("FruitTest20240401.csv").then((data) => {
@@ -28,8 +29,8 @@ d3.select("body")
   .text("Next Page")
   .on("click", () => {
     currentPage++;
-    let pageCounter =  pages.length -1;
-    if (currentPage > pageCounter) { 
+    let pageCounter = pages.length - 1;
+    if (currentPage > pageCounter) {
       currentPage = pageCounter;
     }
     d3.select("svg").selectAll("*").remove();
@@ -41,32 +42,29 @@ d3.select("body")
       d3.select("button#prevButton").style("display", "block");
     }
 
-    if (currentPage >=  (pages.length-1)) {
+    if (currentPage >= pages.length - 1) {
       d3.select("button#nextButton").style("display", "none");
     }
-
-    
   });
 
-//add button that will change pag 
+//add button that will change pag
 d3.select("body")
   .append("button")
   .attr("id", "prevButton")
   .text("Previous Page")
   .style("display", "none")
   .on("click", () => {
-    
     currentPage--;
-    
+
     if (currentPage <= 0) {
       currentPage = 1;
     }
 
     d3.select("svg").selectAll("*").remove();
-    
+
     //clear the svg
     filteredData = pages[currentPage - 1];
-    
+
     buildChart(filteredData);
 
     //hide button if on first page
@@ -77,7 +75,6 @@ d3.select("body")
     if (currentPage == 1) {
       d3.select("button#nextButton").style("display", "block");
     }
-
   });
 
 //Functions
@@ -103,12 +100,17 @@ function buildChart(filteredData) {
     .innerRadius(radius / 2 - 10)
     .outerRadius(radius + 20);
 
+  outerArc = d3
+    .arc()
+    .innerRadius(radius * 1.1)
+    .outerRadius(radius * 1.1);
+
   let arcs = g
-    .selectAll(".arc") // Changed "arc" to ".arc"
+    .selectAll(".arc")
     .data(pie)
     .enter()
     .append("g")
-    .attr("class", "arc"); // Added this line
+    .attr("class", "arc");
 
   arcs
     .append("path")
@@ -117,17 +119,61 @@ function buildChart(filteredData) {
     })
     .attr("d", arc)
     .on("mouseover", mouseOver)
-    .on("mouseout", mouseOut);
+    .on("mouseout", mouseOut)
+    .on("mousemove", mouseMove);
 
-  arcs
+  arcs.append("path").attr("d", outerArc).attr("class", "outerArc");
+
+  var labels = arcs
     .append("text")
     .attr("transform", function (d) {
-      return "translate(" + arc.centroid(d) + ")";
+      var centroid = outerArc.centroid(d);
+      return "translate(" + centroid[0] + "," + centroid[1] + ")";
     })
-    .attr("dy", "0")
+    .attr("text-anchor", function (d) {
+      var midAngle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+      return midAngle > Math.PI ? "end" : "start";
+    })
+    .attr("dy", ".35em")
+    .attr("dx", function (d) {
+      var midAngle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+      return midAngle > Math.PI ? "-0.2em" : "0.2em";
+    })
+    .attr("padding", 10)
     .text(function (d) {
       return d.data.Fruit;
     });
+
+  labels.each(function (d) {
+    var bbox = this.getBBox();
+    d.bottom = bbox.y + bbox.height;
+    d.length = bbox.width;
+  });
+
+
+  arcs
+    .append("polyline")
+    .attr("points", function (d) {
+      var startingPoint = arc.centroid(d);
+      //check if the label is on the right side or left side
+      var midPoint = [outerArc.centroid(d)[0], outerArc.centroid(d)[1]+d.bottom+5];
+      var midAngle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+      var x;
+      if( midAngle > Math.PI)
+        {
+          x = outerArc.centroid(d)[0]-d.length
+        }
+        else {
+          x = outerArc.centroid(d)[0]+d.length
+        }
+      var endingPoint = [x, outerArc.centroid(d)[1]+d.bottom+5];
+    return [startingPoint,midPoint, endingPoint];
+  
+    })
+    .attr("stroke", "black")
+    .attr("fill", "none");
+
+  //g.append("path").attr("d", outerArc).attr("class", "outerArc");
 }
 function dataFilter(data) {
   // Filter the data
@@ -179,4 +225,11 @@ function mouseOut(event, d) {
   d3.select(this).transition().duration(500).attr("d", arc);
 
   tooltip.style("opacity", 0);
+}
+
+function mouseMove(event, d) {
+  // Update tooltip position
+  tooltip
+    .style("left", event.pageX + 10 + "px")
+    .style("top", event.pageY - 10 + "px");
 }
